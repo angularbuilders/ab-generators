ea95728913ef11ed1e48d817f85562c487cb4ef0
Alberto Basalo <albertobasalo@hotmail.com>
2021-04-06 16:49:11 +0200

    fix: css main app name

--- tools/scripts/8-ab-ng-css.sh
+++ tools/scripts/8-ab-ng-css.sh
@@ -1,8 +1,8 @@
 echo "🚧 add CSS";
 echo "add bulma";
 npm i bulma
-echo "this.projects.web.architect.build.options.styles[1] = 'node_modules/bulma/css/bulma.css';"
-json -I -f angular.json -e "this.projects.web.architect.build.options.styles[1] = 'node_modules/bulma/css/bulma.css';"
+echo "this.projects.www.architect.build.options.styles[1] = 'node_modules/bulma/css/bulma.css';"
+json -I -f angular.json -e "this.projects.www.architect.build.options.styles[1] = 'node_modules/bulma/css/bulma.css';"
 git add *
 git commit -m 'chore: add bulma'
 echo "🏠 add CSS";
---
+++ tools/scripts/9-ab-ng-deploy.sh
@@ -0,0 +1,4 @@
+echo "🚧 init nx-workspace";
+npm init nx-workspace REPOSITORY --appName=www --defaultBase=main --interactive=false --linter=eslint --nxCloud=false --npmScope=ORGANIZATION --preset=angular --style=css
+cd REPOSITORY
+echo "🏠 init nx-workspace";
