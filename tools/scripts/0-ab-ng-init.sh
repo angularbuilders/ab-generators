45a6c86270d3966f37a764e3fc4714582f5beb13
Alberto Basalo <albertobasalo@hotmail.com>
2021-04-20 10:31:20 +0200

    feat: global preinstall

--- tools/scripts/0-ab-ng-init.sh
+++ tools/scripts/0-ab-ng-init.sh
@@ -1,4 +1,8 @@
-
+echo "🚧 global prepare";
+npm i -g @angular/cli@latest
+npm i -g @nrwl/cli@latest
+npm install -g json
+echo "🏠 global prepare";
 echo "🚧 init nx-workspace";
 npm init nx-workspace REPOSITORY --appName=www --defaultBase=main --interactive=false --linter=eslint --nxCloud=false --npmScope=ORGANIZATION --preset=angular --style=css
 cd REPOSITORY
