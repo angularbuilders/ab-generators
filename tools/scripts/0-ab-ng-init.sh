902be72b0626f46c011d46491fa5b07798643cf0
Alberto Basalo <albertobasalo@hotmail.com>
2021-05-24 16:53:01 +0200

    feat: initial strict app

--- tools/scripts/0-ab-ng-init.sh
+++ tools/scripts/0-ab-ng-init.sh
@@ -4,7 +4,7 @@ npm i -g @nrwl/cli@latest
 npm i -g json
 echo "🏠 global prepare";
 echo "🚧 init nx-workspace";
-npm init nx-workspace research --appName=init --defaultBase=main --interactive=false --linter=eslint --nxCloud=false --npmScope=rsrch --preset=angular --style=css
+npm init nx-workspace research --appName=www --defaultBase=main e2eTestRunner=none --interactive=false --nxCloud=false --npmScope=rsrch --preset=angular --skipTests=true --style=css --strict=true --unitTestRunner=none
 cd research
 nx migrate latest
 echo "🚧 app intitial configuration ";
